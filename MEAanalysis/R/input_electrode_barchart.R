9d9811eb0fcb9c829b31cad5d7aea9737719a9ea
egordon2 <153216098+egordon2@users.noreply.github.com>
2024-03-14 16:44:27 +0000

    Add data so able to run function documentation examples

---
+++ MEAanalysis/R/input_electrode_barchart.R
@@ -0,0 +1,9 @@
+
+#' @title Input dataset for single_electrode_barchart function
+#'
+#' @description A data set containing electrode burst data with additional columns calculated for various MEA parameters.
+#'
+#' @format A data frame with 8886 rows and 58 variables
+#' @source <https://www.github.com/egordon2/MEAanalysis>
+"input_electrode_barchart"
+
