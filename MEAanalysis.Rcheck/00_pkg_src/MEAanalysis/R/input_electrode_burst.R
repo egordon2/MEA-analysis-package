65ce92d12c8ae4fc706138633e4a0be63ccea1ef
egordon2 <153216098+egordon2@users.noreply.github.com>
2024-04-15 15:18:20 +0100

    create initial test scripts

---
+++ MEAanalysis.Rcheck/00_pkg_src/MEAanalysis/R/input_electrode_burst.R
@@ -0,0 +1,11 @@
+
+#' @title Input dataset for create_electrode_dataset MEA parameter function.
+#'
+#' @description A data set containing electrode burst list data produced by the axis navigator tool.
+#'
+#'
+#' @format ## `
+#' \describe{
+#'   ...
+#' }
+#' @source <https://www.github.com/egordon2/MEAanalysis>
