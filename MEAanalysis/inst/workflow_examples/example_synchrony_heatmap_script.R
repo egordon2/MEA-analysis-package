f9d41e36271691557f10ca4e2cbbcb2708687fe1
egordon2 <153216098+egordon2@users.noreply.github.com>
2024-04-16 10:32:22 +0100

    Update data paths

--- MEAanalysis/inst/workflow_examples/example_synchrony_heatmap_script.R
+++ MEAanalysis/inst/workflow_examples/example_synchrony_heatmap_script.R
@@ -12,9 +12,9 @@
 ### Function should be run for each time interval within a recording which you wish to include as a variable of the heatmap
 ### User needs to update the assigned dataset titles (used for merging datasets), the data_path (direct to neural metric dataset), and heatmap_condition (determines heatmap x axis condition labels)
 
-baseline_data <- create_synchrony_dataset(data_path = "./inst/data/baseline_neuralMetrics.csv",
+baseline_data <- create_synchrony_dataset(data_path = "./data/input_neuralMetric.csv",
                                           heatmap_condition = "Baseline")
-agonist_challenge_data <- create_synchrony_dataset(data_path = "./inst/data/comparison_agonist_challenge_neuralMetrics.csv",
+agonist_challenge_data <- create_synchrony_dataset(data_path = "./data/comparison_agonist_challenge_neuralMetrics.csv",
                                           heatmap_condition = "Agonist Challenge")
 
 ### Following code merges the above datasets by well
